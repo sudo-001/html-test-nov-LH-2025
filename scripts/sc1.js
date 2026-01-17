da82bfdbf7d0b12578941a1d8300bcb4bb3047cd
sudo-001 <carick.atezong@facsciences-uy1.cm>
2026-01-17 11:11:56 +0100

    Ajout de la fonction de calcul de la somme de deux nombres entier sans verifications sur le type et sans metion du type en parametre.

--- scripts/sc1.js
+++ scripts/sc1.js
@@ -1,1 +1,9 @@
 console.log("Bonjour je suis le premier js du dossier");
+
+// -------------- Fonctionalite 1 ----------------
+function add(a, b) {
+  return a + b;
+}
+
+const r = add(2, 3);
+console.log(r);
